322e747d115963d9f98cc0bd8d2b4c39a48fa700
Ben Malohi <u1115837@eng.utah.edu>
2022-09-26 08:44:51 -0600

    testing

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -1,41 +1,164 @@
 /** Class representing a Tree. */
 class Tree {
+  nodes;
   /**
    * Creates a Tree Object
    * Populates a single attribute that contains a list (array) of Node objects to be used by the other functions in this class
    * @param {json[]} json - array of json objects with name and parent fields
    */
-  constructor(json) {
+  constructor(json) 
+  {
+    this.nodes = [];
 
+    for(let node of json )
+    {
+      this.nodes.push(new Node(node.name, node.parent));
+    }
+
+    for(let node of this.nodes)
+    {
+      node.parentNode = this.nodes.find(function(n){
+          n.name === node.parentName
+      });
+    }
   }
 
   /**
    * Assign other required attributes for the nodes.
    */
-  buildTree () {
+  buildTree () 
+  {
     // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
-
+    for(let node of this.nodes)
+    {
+      node.children = this.nodes.filter(function(n){ 
+          if (n.parentName === node.name)
+          {
+            return n; 
+          }
+      });
+    }
+    this.assignLevel(this.nodes.find(function(n){
+      if (n.parentName === 'root')
+      {
+        return n; 
+      }
+    }), 0);
+    this.assignPosition(this.nodes.find(function(n){
+        if (n.parentName === 'root')
+        {
+          return n; 
+        }
+      }), 0);
   }
 
   /**
    * Recursive function that assign levels to each node
    */
-  assignLevel (node, level) {
+  assignLevel (node, level) 
+  {
+    node.level = level;
 
+    for(let child of node.children)
+    {
+      this.assignLevel(child, level + 1);
+    }
   }
 
   /**
    * Recursive function that assign positions to each node
    */
-  assignPosition (node, position) {
+  assignPosition (node, position) 
+  {
+    node.position = position; 
 
+    for(let child of node.children)
+    {
+      let temp = this.assignPosition(child, position);
+
+      if (temp > position + 1)
+      {
+        position = temp;
+      }
+      else 
+      {
+        position += 1; 
+      }
+    }
+    return position;
   }
 
   /**
    * Function that renders the tree
    */
   renderTree () {
+    let x = 50
+    let y = 50
 
+    this.drawLines(this.nodes[0], x, y)
+
+    let g = this.svg.append("g")
+      .attr("transform", function (d, i) {
+        return "translate(0,0)";
+      });
+
+    g.append('circle')
+      .attr("cx", x)
+      .attr("cy", y)
+      .attr("r", 40)
+      .append("text")
+
+    g.append("text")
+      .attr("x", x)
+      .attr("y", y)
+      .attr("class", "label")
+      .attr("text-anchor", "middle")
+      .text(this.nodes[0].name);
+
+    this.drawChildNodes(this.nodes[0], x, y)
   }
 
+  drawLines(parent, pX, pY) {
+    parent.children.forEach(child => {
+      let cX = child.level * 140 + 50
+      let cY = child.position * 85 + 50
+      this.svg.append("line")
+        .attr("x1", pX)
+        .attr("x2", cX)
+        .attr("y1", pY)
+        .attr("y2", cY)
+
+      this.drawLines(child, cX, cY)
+    })
+  }
+
+  drawChildNodes(parent, pX, pY) {
+
+    parent.children.forEach(child => {
+      let cX = child.level * 140 + 50
+      let cY = child.position * 85 + 50
+
+      let g = this.svg.append("g")
+        .attr("transform", function (d, i) {
+          return "translate(0,0)";
+        });
+
+      let circle = g.append('circle')
+        .attr("cx", cX)
+        .attr("cy", cY)
+        .attr("r", 40)
+        .attr("fill", "green")
+        .append("text")
+
+      g.append("text")
+        .attr("x", cX)
+        .attr("y", cY)
+        .attr("text-anchor", "middle")
+        .attr("class", "label")
+        .text(child.name);
+
+      this.drawChildNodes(child, cX, cY)
+    })
+
+  }
 }
--- hw3/script.js
+++ hw3/script.js
@@ -4,12 +4,65 @@ const CHART_HEIGHT = 250;
 const MARGIN = { left: 50, bottom: 20, top: 20, right: 20 };
 const ANIMATION_DUATION = 300;
 
+var svgBarChart, svgLineChart, svgAreaChart, svgScatterPlot,
+  gBarX, gBarY,
+  gLineX, gLineY,
+  gAreaX, gAreaY,
+  gScatterX, gScatterY;
+
+var dataMetric; 
+
 setup();
 
-function setup () {
+function setup() {
 
+  //add event handlers
+  document.getElementById("dataset").addEventListener("change", changeData);
+  document.getElementById("metric").addEventListener("change", changeData);
+  document.getElementById("random").addEventListener("change", changeData);
+  
   // Fill in some d3 setting up here if you need
   // for example, svg for each chart, g for axis and shapes
+
+  //bar chart
+  svgBarChart = d3.select("#Barchart-div")
+    .append("svg")
+    .attr("width", CHART_WIDTH)
+    .attr("height", CHART_HEIGHT)
+    .attr("transform", "translate(" + MARGIN.left + "," + MARGIN.bottom + ")");
+
+  gBarX = this.svgBarChart.append("g").attr("transform", "translate(0," + (CHART_HEIGHT - MARGIN.bottom) + ")");
+  gBarY = this.svgBarChart.append("g").attr("transform", "translate(" + MARGIN.left + ", 0)");
+
+  //line chart
+  svgLineChart = d3.select("#Linechart-div")
+    .append("svg")
+    .attr("width", CHART_WIDTH)
+    .attr("height", CHART_HEIGHT)
+    .attr("transform", "translate(" + MARGIN.left + "," + MARGIN.bottom + ")");
+
+  gLineX = this.svgLineChart.append("g").attr("transform", "translate(0," + (CHART_HEIGHT - MARGIN.bottom) + ")");
+  gLineY = this.svgLineChart.append("g").attr("transform", "translate(" + MARGIN.left + ", 0)");
+
+  //area chart
+  svgAreaChart = d3.select("#Areachart-div")
+    .append("svg")
+    .attr("width", CHART_WIDTH)
+    .attr("height", CHART_HEIGHT)
+    .attr("transform", "translate(" + MARGIN.left + "," + MARGIN.bottom + ")");
+
+  gAreaX = this.svgAreaChart.append("g").attr("transform", "translate(0," + (CHART_HEIGHT - MARGIN.bottom) + ")");
+  gAreaY = this.svgAreaChart.append("g").attr("transform", "translate(" + MARGIN.left + ", 0)");
+
+  //scatter plot
+  svgScatterPlot = d3.select("#Scatterplot-div")
+    .append("svg")
+    .attr("width", CHART_WIDTH)
+    .attr("height", CHART_HEIGHT)
+    .attr("transform", "translate(" + MARGIN.left + "," + MARGIN.bottom + ")");
+
+  gScatterX = this.svgScatterPlot.append("g").attr("transform", "translate(0," + (CHART_HEIGHT - MARGIN.bottom) + ")");
+  gScatterY = this.svgScatterPlot.append("g").attr("transform", "translate(" + MARGIN.left + ", 0)");
 
   changeData();
 }
@@ -18,9 +71,10 @@ function setup () {
  * Render the visualizations
  * @param data
  */
-function update (data) {
+function update(data) {
 
   // ****** TODO ******
+  dataMetric = document.getElementById("metric").value; 
 
 
   // Syntax for line generator.
@@ -50,43 +104,177 @@ function update (data) {
 
   //TODO 
   // call each update function below, adjust the input for the functions if you need to.
+  updateBarChart(data);
+  updateLineChart(data);
+  updateAreaChart(data);
+  updateScatterPlot(data);
 }
 
 /**
  * Update the bar chart
  */
+function updateBarChart(data) {
+  //resets graph
+  svgBarChart.selectAll("rect")
+    .remove()
+    .exit(); 
 
-function updateBarChart () {
+  let xAxis = d3.scaleBand()
+    .domain(data.map(function (date) {
+      return date.date;
+    }))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right]);
 
+  gBarX.call(d3.axisBottom(xAxis)); //appends x-axis to svg
+
+  let yAxis = d3.scaleLinear()
+    .domain([0, d3.max(data, function (value) {
+      return value[dataMetric];
+    })])
+    .range([CHART_HEIGHT - MARGIN.bottom, MARGIN.top]); 
+
+  gBarY.call(d3.axisLeft(yAxis)); //appends y-axis to svg
+  
+  //spit bars
+  svgBarChart.selectAll("#Barchart-div")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", function(d){
+    return xAxis(d.date);
+  })
+  .attr("y", function(d){
+    return yAxis(d[dataMetric]);
+  })
+  .attr("width", xAxis.bandwidth() - 5)
+  .attr("height", function(d){
+    return CHART_HEIGHT - yAxis(d[dataMetric]) - MARGIN.top;
+  }); 
 }
 
 /**
  * Update the line chart
  */
-function updateLineChart () {
+function updateLineChart(data) {
+  //resets graph
+  svgLineChart.selectAll("path")
+    .remove()
+    .exit(); 
 
+  let xAxis = d3.scaleBand()
+    .domain(data.map(function (date) {
+      return date.date;
+    }))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right]);
+
+  gLineX.call(d3.axisBottom(xAxis)); //appends x-axis to svg
+
+  let yAxis = d3.scaleLinear()
+    .domain([0, d3.max(data, function (value) {
+      return value[dataMetric];
+    })])
+    .range([CHART_HEIGHT - MARGIN.bottom, MARGIN.top]); 
+
+  gLineY.call(d3.axisLeft(yAxis)); //appends y-axis to svg
+
+  svgLineChart.append("path")
+  .datum(data)
+  .attr("fill", "none")
+  .attr("stroke", "black")
+  .attr("stroke-width", 1.5)
+  .attr("d", d3.line()
+    .x(function(d){ 
+      return xAxis(d.date); 
+    })
+    .y(function(d){ 
+      return yAxis(d[dataMetric]); 
+    })
+  ); 
+  
 }
 
 /**
  * Update the area chart 
  */
-function updateAreaChart () {
+function updateAreaChart(data) {
+  //resets graph
+  svgAreaChart.selectAll("path")
+    .remove()
+    .exit(); 
 
+  let xAxis = d3.scaleBand()
+    .domain(data.map(function (date) {
+      return date.date;
+    }))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right]);
+
+  gAreaX.call(d3.axisBottom(xAxis)); //appends x-axis to svg
+
+  let yAxis = d3.scaleLinear()
+    .domain([0, d3.max(data, function (value) {
+      return value[dataMetric];
+    })])
+    .range([CHART_HEIGHT - MARGIN.bottom, MARGIN.top]); 
+
+  gAreaY.call(d3.axisLeft(yAxis)); //appends y-axis to svg
+
+  svgAreaChart.append("path")
+  .datum(data)
+  .attr("fill", "black")
+  .attr("stroke", "black")
+  .attr("stroke-width", 1.5)
+  .attr("d", d3.area()
+    .x(function(d){ 
+      return xAxis(d.date); 
+    })
+    .y0(yAxis(0))
+    .y1(function(d){ 
+      return yAxis(d[dataMetric]); 
+    })
+  ); 
 }
 
 /**
  * update the scatter plot.
  */
+function updateScatterPlot(data) {  
+  //resets graph
+  svgScatterPlot.selectAll("circle")
+    .remove()
+    .exit(); 
 
-function updateScatterPlot () {
+  let xAxis = d3.scaleBand()
+    .domain(data.map(function (value) {
+      return value.cases;
+    }))
+    .range([MARGIN.left, CHART_WIDTH - MARGIN.right]);
 
+  gScatterX.call(d3.axisBottom(xAxis)); //appends x-axis to svg
+
+  let yAxis = d3.scaleLinear()
+    .domain([0, d3.max(data, function (value) {
+      return value.deaths; //deaths is always on y, cases for x
+    })])
+    .range([CHART_HEIGHT - MARGIN.bottom, MARGIN.top]); 
+
+  gScatterY.call(d3.axisLeft(yAxis)); //appends y-axis to svg
+
+  svgScatterPlot.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (value) { return xAxis(value.cases); })
+      .attr("cy", function (value) { return yAxis(value.deaths); })
+      .attr("r", 5)
+      .style("fill", "black");
 }
 
 
 /**
  * Update the data according to document settings
  */
-function changeData () {
+function changeData() {
   //  Load the file indicated by the select menu
   const dataFile = d3.select('#dataset').property('value');
 
@@ -126,6 +314,6 @@ function changeData () {
  *  Slice out a random chunk of the provided in data
  *  @param data
  */
-function randomSubset (data) {
+function randomSubset(data) {
   return data.filter((d) => Math.random() > 0.5);
 }
