8002e6fdfb660d3e734f462730a5f5f52692ad71
Ben Malohi <u1115837@eng.utah.edu>
2022-10-06 10:33:28 -0600

    push hw 4 changes

--- hw4/js/line-chart.js
+++ hw4/js/line-chart.js
@@ -8,9 +8,147 @@ class LineChart {
     // Set some class level variables
     this.globalApplicationState = globalApplicationState;
 
+    //2.1 data
+    this.lineChart = d3.select("#line-chart");
+
+    this.filteredData = globalApplicationState.covidData.filter(function (data) {
+      return data.iso_code.includes("OWID");
+    });
+
+    this.groupCovidData = d3.group(this.filteredData, (data) => data.location);
+
+    //2.2 axes
+    this.padding = { left: 80, right: 50 };
+
+    let xAxis = d3.scaleTime()
+      .domain(d3.extent(this.filteredData.map(function (date) { //finds min and max dates and display dates that are evenly distritbuted between them
+        return new Date(date.date);
+      })))
+      .range([this.padding.left, 700]);
+
+    d3.select("#x-axis").call(d3.axisBottom(xAxis).tickFormat(d3.timeFormat("%b %Y"))).attr("transform", "translate(0," + (500 - 20) + ")");
+
+    let yAxis = d3.scaleLinear()
+      .domain([0, d3.max(this.filteredData, function (data) {
+        return +data.total_cases_per_million; // + parses to integer 
+      })])
+      .range([500 - 20, 10]);
+
+    d3.select("#y-axis").call(d3.axisLeft(yAxis)).attr("transform", "translate(" + this.padding.left + ", 0)");
+
+    //2.3 lines
+
+    //lab code
+    var lineColorScale = d3.scaleOrdinal(d3.schemeTableau10).domain(this.groupCovidData.keys());
+
+    this.lineChart.select('#lines')
+      .selectAll('.line')
+      .data(this.groupCovidData)
+      .join('path')
+      .attr('fill', 'none')
+      .attr('stroke', (group) => lineColorScale(group[0]))
+      .attr('stroke-width', 1)
+      .attr('d', function (group) {
+        return d3.line()
+          .x(function (date) { return xAxis(new Date(date.date)) })
+          .y(function (data) { return yAxis(+data.total_cases_per_million) })(group[1]);
+      });
+
+    //2.5 Updating from external cues  
+    this.lineChart.on('mousemove', (event) => {
+      if (event.offsetX > 80 && event.offsetX < innerWidth - 50) {
+        // Set the line position
+        this.lineChart
+          .select('#overlay')
+          .select('line')
+          .attr('stroke', 'black')
+          .attr('x1', event.offsetX)
+          .attr('x2', event.offsetX)
+          .attr('y1', 600 - 10)
+          .attr('y2', 0);
+
+        // Find the relevant data (by date and location)
+        const yearHovered = xAxis.invert(event.offsetX);
+        const filteredHoverData = this.filteredData
+          .filter(function (date){
+            var newDate = new Date(date.date);             
+             return newDate.getMonth() == yearHovered.getMonth() && newDate.getDate() == yearHovered.getDate() && newDate.getFullYear() == yearHovered.getFullYear()
+          
+          })
+          .sort((rowA, rowB) => rowB.total_cases_per_million - rowA.total_cases_per_million);
+
+        this.lineChart.select('#overlay')
+          .selectAll('text')
+          .data(filteredHoverData)
+          .join('text')
+          .text(d => `${d.location}, ${d.total_cases_per_million}`)
+          .attr('x', event.offsetX < (700/2) ? event.offsetX : event.offsetX)
+          .attr('y', (d, i) => 20 * i + 20)
+          .attr('alignment-baseline', 'hanging')
+          .attr('fill', (group) => lineColorScale(group.iso_code));
+      }
+    });
+
+    this.lineChart.on('mouseleave', (event) => {
+      this.lineChart
+        .select("#overlay")
+        .select("line")
+        .attr("stroke", "none"); 
+
+      this.lineChart
+        .select("#overlay")
+        .selectAll("text")
+        .text(""); 
+
+
+    }); 
   }
 
-  updateSelectedCountries () {
+  updateSelectedCountries() {
+    //2.4 interaction
+    if (globalApplicationState.selectedLocations.length == 0) {
+      this.filteredData = globalApplicationState.covidData.filter(function (data) {
+        return data.iso_code.includes("OWID");
+      });
+    }
+    else {
+      this.filteredData = globalApplicationState.covidData.filter(function (data) { //grabs the countries that are in selectedCountries
+        return globalApplicationState.selectedLocations.includes(data.iso_code);
+      });
+    }
 
+    this.groupCovidData = d3.group(this.filteredData, (data) => data.location);
+
+    let xAxis = d3.scaleTime()
+      .domain(d3.extent(this.filteredData.map(function (date) { //finds min and max dates and display dates that are evenly distritbuted between them
+        return new Date(date.date);
+      })))
+      .range([this.padding.left, 700]);
+
+    this.lineChart.select('#lines').selectAll("path").remove().exit();
+    this.lineChart.select('#y-axis').selectAll("g").remove().exit();
+
+    let yAxis = d3.scaleLinear()
+      .domain([0, d3.max(this.filteredData, function (data) {
+        return +data.total_cases_per_million; // + parses to integer 
+      })])
+      .range([500 - 20, 10]);
+
+    d3.select("#y-axis").call(d3.axisLeft(yAxis)).attr("transform", "translate(" + this.padding.left + ", 0)");
+
+    var lineColorScale = d3.scaleOrdinal(d3.schemeTableau10).domain(this.groupCovidData.keys());
+
+    this.lineChart.select('#lines')
+      .selectAll('.line')
+      .data(this.groupCovidData)
+      .join('path')
+      .attr('fill', 'none')
+      .attr('stroke', (group) => lineColorScale(group))
+      .attr('stroke-width', 1)
+      .attr('d', function (group) {
+        return d3.line()
+          .x(function (date) { return xAxis(new Date(date.date)) })
+          .y(function (data) { return yAxis(+data.total_cases_per_million) })(group[1]);
+      });
   }
 }
--- hw4/js/map.js
+++ hw4/js/map.js
@@ -5,16 +5,123 @@ class MapVis {
    * @param globalApplicationState The shared global application state (has the data and the line chart instance in it)
    */
   constructor(globalApplicationState) {
-    this.globalApplicationState = globalApplicationState;
 
+    this.globalApplicationState = globalApplicationState;
+    this.map_svg = d3.select('#map')
     // Set up the map projection
     const projection = d3.geoWinkel3()
       .scale(150) // This set the size of the map
-      .translate([400, 250]); // This moves the map to the center of the SVG
+      .translate([400, 250]); // This moves the map to the center of the SVG.
 
+    this.path = d3.geoPath().projection(projection);
+    let color = d3.scaleQuantize()
+      .range(["#F7B5D3", "#F17EB2", "#EE599C", "#EF1A7A", "#E00065"]);
+
+    let json = topojson.feature(globalApplicationState.mapData, globalApplicationState.mapData.objects.countries);
+
+    color.domain([
+      d3.min(globalApplicationState.covidData, function (d) {
+        return d.total_cases_per_million
+      }),
+      d3.max(globalApplicationState.covidData, function (d) {
+        return d.total_cases_per_million
+      })
+    ]);
+
+    let worldLines = d3.geoGraticule();
+    worldLines.outline();
+
+    this.map_svg.select('#graticules').append("path")
+      .datum(worldLines)
+      .attr('class', 'grat')
+      .attr('d', this.path)
+      .attr('fill', 'none')
+      .attr('stroke', "#DFB2F4")
+      .attr('opacity', "0.8");
+      
+    let covidData = {};
+    this.globalApplicationState.covidData.forEach(function (r) {
+      covidData[r.iso_code] = parseFloat(r.total_cases_per_million)
+    });
+
+    json.features.forEach(function (feature) {
+      feature.properties.value = covidData[feature.id]
+    });
+
+    this.map_svg.select("#countries").selectAll("path")
+      .data(json.features) 
+      .join("path")
+      .attr("d", this.path)
+      .attr("stroke", "#BAA4D5")
+      .style("fill", function (d) {
+        return (isNaN(d.properties.value) ? color(0.01) : color(d.properties.value));
+      })
+      .on("click", this.updateSelectedCountries)
+      .attr("class", function(data){
+        return data.id;
+      }); 
+
+    let defs = this.map_svg.append('defs');
+
+    let gradient = defs.append('linearGradient')
+      .attr('id', 'gradient');
+
+    gradient.append('stop')
+      .attr('class', 'stop-left')
+      .attr('stop-color', '#F7B5D3')
+      .attr('offset', '0');
+
+    gradient.append('stop')
+      .attr('class', 'stop-right')
+      .attr('stop-color', '#E00065')
+      .attr('offset', '1');
+
+    // Use the gradient to set the shape fill, via CSS.
+    this.map_svg.append('rect')
+      .attr('fill', 'url(#gradient')
+      .attr('x', 0)
+      .attr('y', 490)
+      .attr('width', 150)
+      .attr('height', 10);
+
+    this.map_svg.append("text")
+      .text("0")
+      .attr("x", 0)
+      .attr("y", 490);
+
+    this.map_svg.append("text")
+      .text("" + d3.format(".2s")(d3.max(globalApplicationState.covidData, function (d) {
+        return +d.total_cases_per_million;
+      })))
+      .attr("x", 150)
+      .attr("y", 490);
   }
 
-  updateSelectedCountries () {
-
+  updateSelectedCountries() {
+    if(!globalApplicationState.selectedLocations.includes(this.getAttribute("class"))){
+      globalApplicationState.selectedLocations.push(this.getAttribute("class"));
+      d3.selectAll(".Country")
+      .transition()
+      .duration(200)
+      .style("opacity", .5);
+      d3.select(this)
+      .transition()
+      .duration(200)
+      .style("opacity", 1)
+      .style("stroke", "black");
+    }
+    else{
+      globalApplicationState.selectedLocations = globalApplicationState.selectedLocations.filter(country => country!= this.getAttribute("class"));
+      d3.selectAll(".Country")
+      .transition()
+      .duration(200)
+      .style("opacity", .5);
+      d3.select(this)
+      .transition()
+      .duration(200)
+      .style("opacity", 1)
+      .style("stroke", "#BAA4D5");
+    }
+    globalApplicationState.lineChart.updateSelectedCountries(); //call function in linechart to update chart
   }
 }
